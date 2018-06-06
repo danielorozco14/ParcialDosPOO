7e3e83071fe20e355390df336a3b809a8a62722d
Daniel Orozco <00200617@uca.edu.sv>
2018-06-05 23:29:43 -0600

    Se han creado las clases principales de la raza Orcos, falta aun implementarla correctamente

---
+++ src/orcos/Orcos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orcos;
+
+/**
+ *
+ * @author Daniel Alejandro Orozco Orellana <00200617@uca.edu.sv>
+ */
+public class Orcos implements InterfazOrcos {
+    
+    @Override
+    public void Crear() {
+        //AQUI VA EL CODIGO PARA CREAR UN ORCO
+    }
+
+    @Override
+    public void Entrenar() {
+        //AQUI VA EL CODIGO PARA ENTRENAR UN ORCO
+    }
+
+    @Override
+    public void Atacar() {
+        //AQUI VA EL CODIGO PARA DAR LA ORDEN DE ATACAR EL TERRITORIO ENEMIGO
+    }
+
+    @Override
+    public void Defender() {
+        //AQUI VA EL CODIGO PARA DAR LA ORDEN DE DEFENDER EL TERRITORIO ALIADO
+    }
+
+    @Override
+    public void Recolectar() {
+        //AQUI VA EL CODIGO PARA RECOLECTAR LOS ORCOS UNA VEZ 
+        //EL PLASO DE ESPERA DE CONSTRUCCION SE HAYA COMPLETADO Y SE GUARDARAN EN 
+        //LA EDIFICACION CORRECTA
+    }
+    
+}
