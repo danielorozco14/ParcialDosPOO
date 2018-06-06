fa96d1822ef5b1cf149f62cba67ffd57583f8f18
Daniel Orozco <00200617@uca.edu.sv>
2018-06-05 23:29:27 -0600

    Se han creado las clases principales de la raza Necromante, falta aun implementarla correctamente

---
+++ src/necromantes/InterfazNecromantes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package necromantes;
+
+/**
+ *
+ * @author Daniel Alejandro Orozco Orellana <00200617@uca.edu.sv>
+ */
+public interface InterfazNecromantes {
+    void Crear();
+    void Entrenar();
+    void Atacar();
+    void Defender();
+    void Recolectar();
+
+}
---
+++ src/necromantes/SegadoresAlmas.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package necromantes;
+
+/**
+ *
+ * @author Daniel Alejandro Orozco Orellana <00200617@uca.edu.sv>
+ */
+public class SegadoresAlmas implements InterfazNecromantes {
+    
+    @Override
+    public void Crear() {
+        //AQUI VA EL CODIGO PARA CREAR UN SEGADOR DE ALMAS
+    }
+
+    @Override
+    public void Entrenar() {
+        //AQUI VA EL CODIGO PARA ENTRENAR UN SEGADOR DE ALMAS
+    }
+
+    @Override
+    public void Atacar() {
+        //AQUI VA EL CODIGO PARA DAR LA ORDEN DE ATACAR EL TERRITORIO ENEMIGO
+    }
+
+    @Override
+    public void Defender() {
+        //AQUI VA EL CODIGO PARA DAR LA ORDEN DE DEFENDER EL TERRITORIO ALIADO
+    }
+
+    @Override
+    public void Recolectar() {
+        //AQUI VA EL CODIGO PARA RECOLECTAR LOS SEGADORES DE ALMASJ UNA VEZ EL PLAZO DE 
+        //ESPERA DE CONSTRUCCION SE HAYA COMPLETADO Y SE GUARDARAN EN 
+        //LA EDIFICACION CORRECTA
+    }
+}
