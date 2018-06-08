c34cc17450d032bf29aa995ab206af0523259f35
Daniel Orozco <00200617@uca.edu.sv>
2018-06-08 10:48:18 -0600

    Se cambio el nombre de milicia de Caballero de Muerte a Esqueletos

---
+++ src/necromantes/Esqueleto.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package necromantes;
+
+/**
+ *
+ * @author Daniel Alejandro Orozco Orellana <00200617@uca.edu.sv>
+ */
+public class Esqueleto implements InterfazNecromantes
+{
+
+    @Override
+    public void Crear() {
+        //AQUI VA EL CODIGO PARA CREAR UN CABALLERO DE LA MUERTE
+    }
+
+    @Override
+    public void Entrenar() {
+        //AQUI VA EL CODIGO PARA ENTRENAR UN CABALLERO DE LA MUERTE
+    }
+
+    @Override
+    public void Atacar() {
+        //AQUI VA EL CODIGO PARA DAR LA ORDEN DE ATACAR EL TERRITORIO ENEMIGO
+    }
+
+    @Override
+    public void Defender() {
+        //AQUI VA EL CODIGO PARA DAR LA ORDEN DE DEFENDER EL TERRITORIO ALIADO
+    }
+
+    @Override
+    public void Recolectar() {
+        //AQUI VA EL CODIGO PARA RECOLECTAR LOS CABALLEROS UNA VEZ EL PLASO DE 
+        //ESPERA DE CONSTRUCCION SE HAYA COMPLETADO Y SE GUARDARAN EN 
+        //LA EDIFICACION CORRECTA
+    }
+    
+}
