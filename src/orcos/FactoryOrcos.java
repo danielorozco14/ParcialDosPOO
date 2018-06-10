1b84d8f227289bf73857e235be4eb64c1fa99b91
Daniel Orozco <00200617@uca.edu.sv>
2018-06-09 20:51:14 -0600

    Se implementaron los metodos de AbstractFactory

--- src/orcos/FactoryOrcos.java
+++ src/orcos/FactoryOrcos.java
@@ -5,10 +5,33 @@
  */
 package orcos;
 
+import caballerosdesangre.InterfazCaballeros;
+import danielorozco.world.AbstractFactory;
+import necromantes.InterfazNecromantes;
+
 /**
  *
  * @author Daniel Alejandro Orozco Orellana <00200617@uca.edu.sv>
  */
-public class FactoryOrcos {
+public class FactoryOrcos implements AbstractFactory {
+
+    @Override
+    public InterfazCaballeros getCaballero(int type) {
+        return null;
+    }
+
+    @Override
+    public InterfazOrcos getOrcos(int type) {
+        //AQUI SE PONDRA TODAS LAS ACCIONES QUE VA A REALIZAR LA RAZA ORCOS 
+        //CREAR MILICIA,RECOLECTAR RECURSOS,ETC(return Orcos(),return DarkSwordman();
+        return null;
+    }
+
+    @Override
+    public InterfazNecromantes getNecromantes(int type) {
+        return null;
+    }
+
+    
     
 }
