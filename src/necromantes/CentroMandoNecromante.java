86cc6091d85b93c5fc8b779b51a63c5be6479780
Daniel Orozco <00200617@uca.edu.sv>
2018-06-11 15:10:38 -0600

    Implementacion de recoleccion de recursos de necromantes

--- src/necromantes/CentroMandoNecromante.java
+++ src/necromantes/CentroMandoNecromante.java
@@ -5,10 +5,59 @@
  */
 package necromantes;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Daniel Alejandro Orozco Orellana <00200617@uca.edu.sv>
  */
 public class CentroMandoNecromante {
-    
+    private static int vidaHQ;
+    private static CentroMandoNecromante hq;
+
+    public int getVidaHQ() {
+        return vidaHQ;
+    }
+
+    public void setVidaHQ(int vidaHQ) {
+        this.vidaHQ = vidaHQ;
+    }
+
+    private CentroMandoNecromante() {
+    }
+
+    public static CentroMandoNecromante getInstance() {
+        if (hq == null) {
+            hq = new CentroMandoNecromante();
+        }
+        return hq;
+    }
+
+    public void CentroMandoCaballero() {
+        Scanner opcion = new Scanner(System.in);
+        setVidaHQ(20000);
+        Recursos tipoRecurso = new Recursos();
+        System.out.println("******** CENTRO DE MANDO CABALLEROS DE SANGRE *********");
+        System.out.println("\t\t1. Recolectar Recursos Cristal");
+        System.out.println("\t\t2. Recolectar Recursos Almas");
+        System.out.println("\t\t3. Recolectar Recursos Huesos");
+
+        switch (opcion.nextInt()) {
+            case 1:
+                System.out.println("Cantidad de cristal a Recolectar: " + Recursos.getCantidadCristal());
+                tipoRecurso.RecolectarCristales(Recursos.getCantidadCristal());
+                break;
+            case 2:
+                System.out.println("Cantidad de almas a Recolectar: " + Recursos.getCantidadAlmas());
+                tipoRecurso.RecolectarAlmas(Recursos.getCantidadAlmas());
+                break;
+
+            case 3:
+                System.out.println("Cantidad de huesos a Recolectar: " + Recursos.getCantidadHuesos());
+                tipoRecurso.RecolectarHuesos(Recursos.getCantidadHuesos());
+                break;
+        }
+
+    }
+
 }
